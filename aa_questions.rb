bb89e8041dacb8c337d7dd5e0d5ee8d2af3aab66
Tristan Angeles <setswipe@gmail.com>
2021-09-08 11:59:37 -0400

    classes completed but not tested

--- aa_questions.rb
+++ aa_questions.rb
@@ -30,4 +30,88 @@ class User
         data = QuestionsDatabase.instance.execute("SELECT * FROM users WHERE id = #{id}")
         data.map{|datum| User.new(datum)}
     end
-end
+
+    def self.find_by_name(fname, lname)
+        data = QuestionsDatabase.instance.execute("SELECT * FROM users WHERE fname = #{fname} AND lname = #{lname} ")
+        data.map { |datum| User.new(datum) }
+    end
+end # End User Class
+
+class Question 
+    def self.all
+        data = QuestionsDatabase.instance.execute("SELECT * FROM questions")
+        data.map{|datum| Question.new(datum)}
+    end
+
+    def initialize(options)
+        @id = options['id']
+        @title = options['title']
+        @body = options['body']
+        @user_id = options['user_id']
+    end
+
+    def self.find_by_id(id)
+        data = QuestionsDatabase.instance.execute("SELECT * FROM questions WHERE id = #{id}")
+        data.map{|datum| Question.new(datum)}
+    end
+
+end #end questions
+
+class Reply
+     def self.all
+        data = QuestionsDatabase.instance.execute("SELECT * FROM replies")
+        data.map{|datum| Reply.new(datum)}
+    end
+
+    def initialize(options)
+        @id = options['id']
+        @question_id = options['question_id']
+        @body = options['body']
+        @user_id = options['user_id']
+        @parent_reply_id = options['parent_reply_id']
+    end
+
+    def self.find_by_id(id)
+        data = QuestionsDatabase.instance.execute("SELECT * FROM replies WHERE id = #{id}")
+        data.map{|datum| Reply.new(datum)}
+    end
+
+end # end Reply Class
+
+class QuestionFollow
+    def self.all
+        data = QuestionsDatabase.instance.execute("SELECT * FROM question_follows")
+        data.map{|datum| QuestionFollow.new(datum)}
+    end
+
+    def initialize(options)
+        @id = options['id']
+        @question_id = options['question_id']
+        @user_id = options['user_id']
+    end
+
+    def self.find_by_id(id)
+        data = QuestionsDatabase.instance.execute("SELECT * FROM question_follows WHERE id = #{id}")
+        data.map{|datum| QuestionFollow.new(datum)}
+    end
+
+end #end QuestionFollows.class
+
+class QuestionLike
+    def self.all
+        data = QuestionsDatabase.instance.execute("SELECT * FROM question_likes")
+        data.map{|datum| QuestionLike.new(datum)}
+    end
+
+    def initialize(options)
+        @id = options['id']
+        @question_id = options['question_id']
+        @user_id = options['user_id']
+    end
+
+    def self.find_by_id(id)
+        data = QuestionsDatabase.instance.execute("SELECT * FROM question_likes WHERE id = #{id}")
+        data.map{|datum| QuestionLike.new(datum)}
+    end
+
+end #end QuestionLike
